bafde0621afa3589eee2cff9277dc638c81e78e1
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-06-24 16:44:51 +0300

    Add redux actions, reducers, store. Update App and main.jsx. Add whole structure of the project. Add dependencies.

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/catalog">Catalog</Link>
+            <Link to="/favorites">Favorites</Link>
+        </nav>
+    )
+}
+
+export default Navbar;

a51a02942cce2fdc9bb1fd1fbccff142df54fd8d
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-06-28 18:23:22 +0300

    Fix bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
-import './App.css'
-import Navbar from './components/Navbar.jsx';
-import AppRoutes from './routes/routes.jsx';
+// import './App.css'
+// import Navbar from './components/Navbar.jsx';
+// import AppRoutes from './routes/routes.jsx';
+import { lazy } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
 
-function App() {
+const Home = lazy(() => import("./pages/Home"));
+const Catalog = lazy(() => import("./pages/Catalog"));
+const Favorites = lazy(() => import("./pages/Favorites"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
+const App = () => {
     return (
-        <>
-            <div>
-                <Navbar />
-                <AppRoutes />
-            </div>
-        </>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/404" />} />
+        </Routes>
     )
 }
 

5285f0a5a2881f2f455b23bdd1f214fee2425be2
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-07-01 14:27:46 +0300

    Updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 // import React from "react";
+import Catalog from "./Catalog";
+// import { Navigate } from "react-router-dom";
 
 const Home = () => {
     return (
         <div>
             <h1>Welcome to our Camp Trail Service</h1>
             <p>We offer the best camper vans for your travel needs in Ukraine.</p>
-            
+            <div>
+                <Catalog/>
+            </div>
         </div>
     )
 }

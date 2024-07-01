8ec3a97c43105415c5847d8b06582b3c48136cb7
Tetiana Bidnenko <tnbdnnk@gmail.com>
2024-07-01 10:07:26 +0000

    Updates

--- assets/NotFound-DUnCuApd.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Cojq-mmK.js";const r=()=>o.jsx("div",{children:o.jsx("h1",{children:"404 - Not Found"})});export{r as default};
---
+++ assets/NotFound-D_EIplW4.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-LxPitM6T.js";const r=()=>o.jsx("div",{children:o.jsx("h1",{children:"404 - Not Found"})});export{r as default};

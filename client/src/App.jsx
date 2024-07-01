1c05cd4a9a98d2cfbd0f53e28bafbdcf261b9ec5
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-07-01 13:06:29 +0300

    Updated App, main, index.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,26 @@
-// import './App.css'
-// import Navbar from './components/Navbar.jsx';
-// import AppRoutes from './routes/routes.jsx';
-import { lazy } from "react";
-import { HashRouter as Route, Routes, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import {
+  HashRouter as
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 const Home = lazy(() => import("./pages/Home"));
 const Catalog = lazy(() => import("./pages/Catalog"));
 const Favorites = lazy(() => import("./pages/Favorites"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
-const App = () => {
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/catalog" element={<Catalog />} />
-            <Route path="/favorites" element={<Favorites />} />
-            <Route path="/404" element={<NotFound />} />
-            <Route path="*" element={<Navigate to="/404" />} />
-        </Routes>
-    
-}
+const App = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/catalog" element={<Catalog />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/404" element={<NotFound />} />
+      <Route path="*" element={<Navigate to="/404" />} />
+    </Routes>
+  </Suspense>
+);
 
 export default App;
